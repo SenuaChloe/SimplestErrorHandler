adb618eeaa09358ef7a3f744593304f7a5eca900
Senua <chloe.lourseyre@gmail.com>
2022-01-13 10:52:28 +0100

    Begining of version 2 -- without recursion
    Recursion was actually usless in v1.0

--- ErrorHandler.h
+++ ErrorHandler.h
@@ -3,35 +3,14 @@
 #include <iostream>
 #include <sstream>
 
-template<typename TExceptionType, typename ...TArgs>
-concept ErrorHandlerTemplatedTypesConstraints = requires(std::string s, std::ostringstream oss, TArgs... args)
-{
-    TExceptionType(s); // TExceptionType must be constructible using a std::string
-    (oss << ... << args); // All args must be streamable
-};
-
-class ErrorHandler
-{
-    ErrorHandler(); // Private constructor -- this is a full-static class
-    
-    template<typename TExceptionType, typename THead>
-    static void raise_error_recursion(std::ostringstream & error_string_stream, const THead & arg_head)
+namespace ErrorHandler
+{   
+    template<typename TExceptionType, typename ...TArgs>
+    concept ErrorHandlerTemplatedTypesConstraints = requires(std::string s, std::ostringstream oss, TArgs... args)
     {
-        error_string_stream << arg_head;
-        const std::string current_error_str = error_string_stream.str();
-
-        std::cerr << current_error_str << std::endl;
-        throw TExceptionType(current_error_str);
-    }
-
-    template<typename TExceptionType, typename THead, typename ...TTail>
-    static void raise_error_recursion(std::ostringstream & error_string_stream, const THead & arg_head, const TTail & ...arg_tail)
-    {
-        error_string_stream << arg_head;
-        raise_error_recursion<TExceptionType>(error_string_stream, arg_tail...);
-    }
-
-public:
+        TExceptionType(s); // TExceptionType must be constructible using a std::string
+        (oss << ... << args); // All args must be streamable
+    };
 
     class BasicException : public std::exception
     {
@@ -45,15 +24,18 @@ public:
 
     template<typename TExceptionType = BasicException, typename ...TArgs>
     requires ErrorHandlerTemplatedTypesConstraints<TExceptionType, TArgs...>
-    static void raise_error(const TArgs & ...args)
+    void raise_error(const TArgs & ...args)
     {
-        std::ostringstream error_string_stream;
-        raise_error_recursion<TExceptionType>(error_string_stream, args...);
+        std::ostringstream oss;
+        (oss << ... << args);
+        const std::string error_str = oss.str();
+        std::cerr << error_str << std::endl;
+        throw TExceptionType(error_str);
     }
 
     template<typename TExceptionType = BasicException, typename ...TArgs>
     requires ErrorHandlerTemplatedTypesConstraints<TExceptionType, TArgs...>
-    static void assert(bool predicate, const TArgs & ...args)
+    void assert(bool predicate, const TArgs & ...args)
     {
         if (!predicate)
             raise_error<TExceptionType>(args...);
